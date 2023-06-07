c41c05f6266c652fc899cbc49e8e51d214f7b4ff
sgorgun <sergei.gorgun@gmail.com>
2023-06-07 14:00:36 +0300

    Begin

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿var cookiesRecipesApp = new CookiesRecipesApp();
+cookiesRecipesApp.Run();
+
+public class CookiesRecipesApp
+{
+    private readonly RecpesRepository _recpesRepository;
+    private readonly RecpesUserInteraction _recpesUserInteraction;
+
+    /// <summary>
+    /// Inicialise filds in class.
+    /// </summary>
+    /// <param name="recpesRepository">Recepies storage.</param>
+    /// <param name="recpesUserInteraction">Ineraction with recepies. Like display, print and so on.</param>
+    public CookiesRecipesApp(RecpesRepository recpesRepository, RecpesUserInteraction recpesUserInteraction)
+    {
+        _recpesRepository = recpesRepository;
+        _recpesUserInteraction = recpesUserInteraction;
+    }
+
+    public void Run()
+    {
+        throw new NotImplementedException();
+    }
+}
