b92adf368f78cd4630ca85495b154374d1374cde
sgorgun <115941193+sgorgun@users.noreply.github.com>
2023-06-07 15:20:23 +0300

    program structure ready

--- Program.cs
+++ Program.cs
@@ -17,12 +17,29 @@ public class CookiesRecipesApp
         _recpesUserInteraction = recpesUserInteraction;
     }
 
+    /// <summary>
+    /// Main program metod.
+    /// </summary>
     public void Run()
     {
-        throw new NotImplementedException();
+        var allRecipes = _recpesRepository.Read(filePath); // Step 1: Reading all recipes. TODO: make filePath.
+        _recpesUserInteraction.PrintExistingRecipes(allRecipes); // Step 2: Display on the screen all recepies.
+        _recpesUserInteraction.PromtToCreateRecipe(); // Step 3: Prompting the user to create recipy.
+        var ingredients = _recpesUserInteraction.ReadIngredientsFromUser(); //Step 4: // Reading ingredirnts from user.
 
-        Console.WriteLine("Hi!");
-    }
+        if (ingredients.Count > 0) // Check: is user selected any ingredients?
+        {
+            var recepes = new Recipy(ingredients); // Using ingrediens for creating recipe.
+            allRecipes.Add(recepe); // Add recepe in recipes list.
+            _recpesRepository.Write(filePath, allRecipes) // Write recipes to file.
+            _recpesUserInteraction.ShowMessage("Recipe added."); //Show messade about adding.
+            _recpesUserInteraction.ShowMessage(recepe.ToString()); // Show recepe.
+        }
+        else // Message if recepies are not added.
+        {
+            _recpesUserInteraction.SowMessage("No ingredients has been selected. Recipe will not be saved.")
+        }
 
-    
+        _recpesUserInteraction.Exit();       
+    }   
 }
